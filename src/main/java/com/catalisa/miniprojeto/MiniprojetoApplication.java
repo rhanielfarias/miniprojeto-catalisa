5bfcb2e5066db770dd37117e1f605ac5c9b3c6c6
rhanielfarias <rhanielfarias@gmail.com>
2022-09-27 09:42:36 -0300

    Criando o projeto Spring

---
+++ src/main/java/com/catalisa/miniprojeto/MiniprojetoApplication.java
@@ -0,0 +1,13 @@
+package com.catalisa.miniprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojetoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojetoApplication.class, args);
+	}
+
+}
